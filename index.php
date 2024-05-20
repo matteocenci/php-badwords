121c3f1717b9acd1e5014fac11020f8b8a4e4de1
matteocenci <matteocenci01@gmail.com>
2024-05-20 16:01:08 +0200

    feat: init project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Inserisci il paragrafo e la parola da censurare</h1>
+    <form action="process.php" method="GET">
+        <label for="paragraph">Paragrafo:</label><br>
+        <textarea id="paragraph" placeholder="Scrivi qui il paragrafo..."></textarea>
+        <label for="word">Parola da censurare:</label><br>
+        <input type="text" id="word"><br>
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
